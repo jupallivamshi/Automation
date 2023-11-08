736bd59b6ebf18ed955877e136614e2d44cc0f1d
bandi <vamshikrishnam007@gmail.com>
2023-11-08 11:03:19 +0530

    Commited by vamshi

---
+++ src/gitproject/demo.java
@@ -0,0 +1,15 @@
+package gitproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.edge.driver","C:\\Users\\bandi\\Downloads\\edgedriver_win64 (1)\\msedgedriver.exe");
+		WebDriver driver  = new EdgeDriver();
+		driver.get("https://www.google.com/");
+	}
+
+}
